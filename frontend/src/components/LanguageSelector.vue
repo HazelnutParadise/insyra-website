50aace4911334b12017215718951f42065750ec8
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-10-31 18:46:44 +0800

    Update LanguageSelector.vue

--- frontend/src/components/LanguageSelector.vue
+++ frontend/src/components/LanguageSelector.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="language-selector">
-      <el-select-v2
+  <div class="language-selector">
+    <div class="select-wrapper">
+      <i class="fa fa-globe select-icon"></i>
+      <select 
         v-model="value"
-        :options="options"
-        placeholder="Please select"
-        size="large"
-        style="width: 240px"
-        @change="emitLanguageChange"
-    />
+        class="language-select"
+        @change="emitLanguageChange($event.target.value)"
+      >
+        <option 
+          v-for="option in options" 
+          :key="option.value"
+          :value="option.value"
+        >
+          {{ option.label }}
+        </option>
+      </select>
     </div>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-
 
 export default {
   props: {
@@ -29,7 +36,7 @@ export default {
       currentLanguage = initials[0]
     }
        
-    const value = ref(currentLanguage) // 預設選擇繁體中文
+    const value = ref(currentLanguage)
     const options = initials.map((lang) => ({
       value: lang,
       label: lang,
@@ -37,7 +44,6 @@ export default {
 
     const emitLanguageChange = (lang) => {
       localStorage.setItem('language', lang);
-      // emit('language-changed', lang); // 發射事件
       location.reload();
     };
 
@@ -51,9 +57,36 @@ export default {
 </script>
 
 <style scoped>
-.example-showcase .el-select-v2 {
-  margin-right: 20px;
-  
+.select-wrapper {
+  position: relative;
+  display: inline-flex;
+  align-items: center;
 }
 
+.select-icon {
+  position: absolute;
+  left: 10px;
+  color: #606266;
+  z-index: 1;
+}
+
+.language-select {
+  width: 240px;
+  padding: 8px;
+  padding-left: 35px;
+  font-size: 16px;
+  border: 1px solid #dcdfe6;
+  border-radius: 4px;
+  background-color: white;
+  cursor: pointer;
+}
+
+.language-select:focus {
+  outline: none;
+  border-color: #409eff;
+}
+
+.language-select option {
+  padding: 8px;
+}
 </style>
